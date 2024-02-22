1cb2e98124acb25fe598c221cfd95f2f5511162f
Sujas Aggarwal <ce1221047@iitd.ac.in>
2024-02-23 02:53:58 +0530

    Completed Program

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,24 @@ use flate2::Compression; // An Enum of Compression Levels
 use std::env::args; // To Access CLI Arguements
 use std::fs::File; // To Access Files
 use std::io::copy; // Copy Data from One I/O to Other
-use std::io::BufReader; // Wrapper for File or Network Streams to Streamline
+use std::io::BufReader;// Wrapper for File or Network Streams to Streamline
 use std::time::Instant; // To measure Time Taken
 
-
-fn main(){
-    println!("Hello World");
+fn main() {
+    if args().len() != 3 {
+        eprintln!("Invalid Input\nExpected Use: 'Source' 'Target'");
+        return;
+    }
+    let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
+    let output = File::create(args().nth(2).unwrap()).unwrap();
+    let mut encoder = GzEncoder::new(output, Compression::default());
+    let timer = Instant::now();
+    copy(&mut input, &mut encoder).unwrap();
+    let output = encoder.finish().unwrap();
+    println!(
+        "Source len: {:#?}",
+        input.get_ref().metadata().unwrap().len()
+    );
+    println!("Target len: {:#?}", output.metadata().unwrap().len());
+    println!("Time Elapsed: {:#?}", timer.elapsed());
 }
